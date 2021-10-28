45603f3278cbdba30738d3c43671614c796c2af2
muhammadrafnas <rafnaszzz8164@gmail.com>
2021-10-29 00:42:47 +0530

    new

--- script.js
+++ script.js
@@ -10,11 +10,4 @@ var currentScrollPos = window.pageYOffset;
   }
   prevScrollpos = currentScrollPos;
 }
-$(document).ready(function(){
-    $(window).scroll(function(){
-        if($(window).scrollTop() > $(window).height()){
-            $(".menu").css({"background-color":"transparent"});   
-        }
-        else{
-            $(".menu").css({"background-color":"white"});
-        }
+
